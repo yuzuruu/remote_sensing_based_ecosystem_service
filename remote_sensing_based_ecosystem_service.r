02bdd0887c7f85c47c83527f284e01ba6fa08a5b
Yuzuru Utsunomiya <yuzuru@nagasaki-u.ac.jp>
2019-10-11 11:42:31 +0900

    added colors of points by total investment, income, and area to maps

--- remote_sensing_based_ecosystem_service.r
+++ remote_sensing_based_ecosystem_service.r
@@ -10,6 +10,7 @@
 # ---- load.library ----
 library(rgdal)
 library(ggspatial)
+library(ggsn)
 library(tidyverse)
 library(viridis)
 library(viridisLite)
@@ -86,20 +87,22 @@ hh.2010.sub %>%
   ggplot(aes(x = total_area, colour = disct))+
   geom_line(stat = "density", size = 1)+
   scale_color_viridis_d(option = "viridis", aesthetics = "colour") +
-  labs(x = "Total area", y = "Density", colour = "District") +
+  labs(x = "Total area", y = "Density", colour = "District", ) +
   theme_classic()
 # save the density plot
 ggsave("hh.2010.sub.density.pdf")
 #
 # END ---
 
-# ---- plot map and survey points ----
+# ---- map.data ----
 # To download sf file, please refer to the following website.
 # The sf file is much lighter than .shp and suitable for R.
 # https://gadm.org/download_country_v3.html
 #
 # read the sf file
-vnm.adm <- readRDS("gadm36_VNM_3_sf.rds")
+vnm.adm.01 <- readRDS("gadm36_VNM_1_sf.rds")
+vnm.adm.02 <- readRDS("gadm36_VNM_2_sf.rds")
+vnm.adm.03 <- readRDS("gadm36_VNM_2_sf.rds")
 # Pick up data of Ca Mau province
 # Somehow it does not work.
 vnm.adm %>% dplyr::filter(NAME_1 == "Ca Mau")
@@ -108,33 +111,45 @@ vnm.adm.cm <-
   vnm.adm %>% 
   dplyr::filter(GID_1 == "VNM.13_1")
 
-# Write a map of administrative boundaries and overwrap survey points
+# Draw a map of administrative boundaries and overwrap survey points
 # We use OpenStreet map for base layer.
 # We can use google map. It, however, requires API when we use that many times.
 # Referring to the following website, we are able to switch 
 # type of map.
 # https://wiki.openstreetmap.org/wiki/Tile_servers
 
-map.osm.vnm.cm <- 
+# ---- map.total.area ----
+# colouring by total area
+# total area
+map.osm.vnm.cm.total.area <- 
   ggplot(vnm.adm.cm) + 
   annotation_map_tile(zoomin = 0, # This argument is adjusted automatically even if we fix it. 
                       type = "https://a.tile.openstreetmap.org/${z}/${x}/${y}.png"
   ) + 
   # adjust administrative boundaries' color, density, and lines' width.
-  geom_sf(alpha = 0.3, colour = "grey60", size = 0.5) +
+  geom_sf(alpha = 0.3, 
+          colour = "grey60", 
+          size = 0.5
+          ) +
   # Adjust area
   xlim(104.6, 105.5) +
   ylim(8.5, 9.0) +
   # Overwrap survey point using survey results
   geom_point(
     data = hh.2010.sub,
-    aes(x = lon, y = lat),
-    fill = "yellow",
+    aes(x = lon, y = lat, fill = total_area),
     colour = "grey28",
     shape = 21,
     size = 2.5
   ) +
-  labs(x = "Longitude", y = "Latitude", caption = "\U00a9 OpenStreetMap contributors") +
+  scale_fill_viridis_c(option = "viridis",
+                       begin = 1,
+                       end = 0
+                       ) +
+  labs(x = "Longitude", 
+       y = "Latitude",
+       caption = "\U00a9 OpenStreetMap contributors"
+       ) +
   theme_minimal() + 
   # adjust scalebar's preferences
   ggsn::scalebar(x.min = 105.0,
@@ -152,13 +167,125 @@ map.osm.vnm.cm <-
                  box.fill = c("grey30", "white"), # left and right
                  box.color = "white",
                  st.color = "white"
-  ) +
-  north(data = vnm.adm.cm, location = "topleft") # direction
-
+  ) 
 # save the map
-ggsave("map.osm.vnm.cm.pdf" ,
-       plot = map.osm.vnm.cm
+ggsave("map.osm.vnm.cm.total.area.pdf" ,
+       plot = map.osm.vnm.cm.total.area
        )
+
+# ---- map.total.income ----
+# colouring by total income
+map.osm.vnm.cm.total.inco <- 
+  ggplot(vnm.adm.cm) + 
+  annotation_map_tile(zoomin = 0, # This argument is adjusted automatically even if we fix it. 
+                      type = "https://a.tile.openstreetmap.org/${z}/${x}/${y}.png"
+  ) + 
+  # adjust administrative boundaries' color, density, and lines' width.
+  geom_sf(alpha = 0.3, 
+          colour = "grey60", 
+          size = 0.5
+  ) +
+  # Adjust area
+  xlim(104.6, 105.5) +
+  ylim(8.5, 9.0) +
+  # Overwrap survey point using survey results
+  geom_point(
+    data = hh.2010.sub,
+    aes(x = lon, y = lat, fill = total_inco),
+    colour = "grey28",
+    shape = 21,
+    size = 2.5
+  ) +
+  scale_fill_viridis_c(option = "viridis",
+                       begin = 1,
+                       end = 0
+  ) +
+  labs(x = "Longitude", 
+       y = "Latitude",
+       caption = "\U00a9 OpenStreetMap contributors"
+  ) +
+  theme_minimal() + 
+  # adjust scalebar's preferences
+  ggsn::scalebar(x.min = 105.0,
+                 x.max = 105.4,
+                 y.min = 8.55,
+                 y.max = 8.60, 
+                 dist_unit = "km",
+                 dist = 20, 
+                 st.size = 4,
+                 st.dist = 0.25,
+                 height = 0.25,
+                 model = "WGS84", 
+                 transform = TRUE,
+                 location = "bottomright",
+                 box.fill = c("grey30", "white"), # left and right
+                 box.color = "white",
+                 st.color = "white"
+  ) 
+# save the map
+ggsave("map.osm.vnm.cm.total.inco.pdf" ,
+       plot = map.osm.vnm.cm.total.inco
+)
+#
+# END ---
+
+# ---- map.total.investment ----
+# colouring by total investment
+map.osm.vnm.cm.total.invest <- 
+  ggplot(vnm.adm.cm) + 
+  annotation_map_tile(zoomin = 0, # This argument is adjusted automatically even if we fix it. 
+                      type = "https://a.tile.openstreetmap.org/${z}/${x}/${y}.png"
+  ) + 
+  # adjust administrative boundaries' color, density, and lines' width.
+  geom_sf(alpha = 0.3, 
+          colour = "grey60", 
+          size = 0.5
+  ) +
+  # Adjust area
+  xlim(104.6, 105.5) +
+  ylim(8.5, 9.0) +
+  # Overwrap survey point using survey results
+  geom_point(
+    data = hh.2010.sub,
+    aes(x = lon, y = lat, fill = total_invest),
+    colour = "grey28",
+    shape = 21,
+    size = 2.5
+  ) +
+  scale_fill_viridis_c(option = "viridis",
+                       begin = 1,
+                       end = 0
+  ) +
+  labs(x = "Longitude", 
+       y = "Latitude",
+       caption = "\U00a9 OpenStreetMap contributors"
+  ) +
+  theme_minimal() + 
+  # adjust scalebar's preferences
+  ggsn::scalebar(x.min = 105.0,
+                 x.max = 105.4,
+                 y.min = 8.55,
+                 y.max = 8.60, 
+                 dist_unit = "km",
+                 dist = 20, 
+                 st.size = 4,
+                 st.dist = 0.25,
+                 height = 0.25,
+                 model = "WGS84", 
+                 transform = TRUE,
+                 location = "bottomright",
+                 box.fill = c("grey30", "white"), # left and right
+                 box.color = "white",
+                 st.color = "white"
+  ) 
+# save the map
+ggsave("map.osm.vnm.cm.total.invest.pdf" ,
+       plot = map.osm.vnm.cm.total.invest
+)
 
 #
 # END ---
+
+
+
+
